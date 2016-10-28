3937d63b14fc40d599c385af540721e87d76ddeb
Samuel Vincent <samuel1.vincent101@gmail.com>
2016-10-28 14:37:17 -0400

    dfsdafsdfdsfasfsad

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,15 +9,16 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
+        {         
             for (int i = 0; i <= 200; i++)
                 {
-                    Console.WriteLine("Bonjour ma gang de crottée");
+                    Console.WriteLine("Bonjour ma gang de droggé");
                 }
             Console.WriteLine("allo");
+          
         }
 
-        void AfficherQuelqueChoseDeLaid()
+        private void AfficherQuelqueChoseDeLaid()
         {
             for (int i = 0; i < 1000; i++)
                 {
